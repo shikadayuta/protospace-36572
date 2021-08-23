3e04e9672bcd1cbb12aa6eaeaf221bfa08825f05
shikadayuta <sikada1218@gmail.com>
2021-08-23 10:59:53 +0900

    カラムの修正

--- db/migrate/20210816022528_devise_create_users.rb
+++ db/migrate/20210816022528_devise_create_users.rb
@@ -4,12 +4,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               
+      t.string :name               
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.text   :profile,            
-      t.text   :occupation,         
-      t.text   :position,           
+      t.text   :profile            
+      t.text   :occupation         
+      t.text   :position           
 
       ## Recoverable
       t.string   :reset_password_token
